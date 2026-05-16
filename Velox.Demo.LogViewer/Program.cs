57dacbd13d4ea7eea89b918567771127fa4b6f76
Andrei Pana <andreipana@outlook.com>
2026-05-16 17:17:34 +0200

    Add new Demo.LogViewer project

---
+++ Velox.Demo.LogViewer/Program.cs
@@ -0,0 +1,16 @@
+using Velox;
+
+namespace Velox.Demo.LogViewer
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var application = new Application();
+            var window = application.CreateWindow();
+            window.Size = (1024, 768);
+            application.Run();
+        }
+    }
+}
